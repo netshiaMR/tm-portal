c0af5804c6f046fc220573ccbfda15c69fa46a4d
start.vaadin.com <no-reply@vaadin.com>
2023-03-17 14:57:38 +0000

    Generated project

---
+++ src/main/java/com/space/tmportal/data/Role.java
@@ -0,0 +1,5 @@
+package com.space.tmportal.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/space/tmportal/data/service/SamplePersonRepository.java
@@ -0,0 +1,12 @@
+package com.space.tmportal.data.service;
+
+import com.space.tmportal.data.entity.SamplePerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SamplePersonRepository
+        extends
+            JpaRepository<SamplePerson, Long>,
+            JpaSpecificationExecutor<SamplePerson> {
+
+}
